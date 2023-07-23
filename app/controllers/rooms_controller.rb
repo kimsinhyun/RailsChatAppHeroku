a714aab7ee37682cb27b3de1a5ce974d1b0f58a8
sinhyun Kim <ksh415622@naver.com>
2023-07-23 13:13:27 +0900

    feature: add Message Model

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -11,6 +11,19 @@ class RoomsController < ApplicationController
     @room = Room.create!(create_params)
   end
 
+  def show
+    @single_room = Room.find(params[:id])
+
+    @room = Room.new
+    @rooms = Room.public_rooms
+
+    @message = Message.new
+    @messages = @single_room.messages.order(created_at: :asc).includes(:user)
+
+    @users = User.all_expect(current_user)
+    render 'rooms/index'
+  end
+
   def create_params
     params.require(:room).permit(:name, :is_private)
   end
